b270f6773cf077ae2ae84949379664bcc93b304a
Chase Saca <chasesaca@chases-mbp.dhcp.nd.edu>
2022-01-18 11:45:57 -0500

    checking to see if Chase's changes affect the hosted website

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -16,7 +16,7 @@ export default function Home() {
         <Col md="6" className="LeftSide">
           <Container>
             <p>This is a basic react template set up with react router and react bootstrap for quick react project startup.</p>
-            <p>Change the Routes.js routes and add new containers to implement new pages.</p>
+            <p>Change the Routes.js routes and add new containers to implement new pages. Hi this is Chase. :)</p>
           </Container>
         </Col>
         <Col md="6">
