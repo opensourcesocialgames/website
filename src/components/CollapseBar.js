1b74b1e34830e1521db7f3e2e4808a88279b34bd
Chase Saca <chasesaca@Chases-MacBook-Pro.local>
2022-02-01 23:34:54 -0500

    fixed unused variable bug

--- src/components/CollapseBar.js
+++ src/components/CollapseBar.js
@@ -3,7 +3,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import { LinkContainer } from "react-router-bootstrap";
