484a2afda38c16c7c9ad64908c6bcb0f59cb14c3
Jack Gordley <jgordley99@gmail.com>
2022-02-07 15:33:46 -0500

    Adding installation page placeholder

--- src/containers/Installation.js
+++ src/containers/Installation.js
@@ -1,12 +1,24 @@
 import React from "react";
+import BlobFishParty from "../images/blobfish.png";
 
-export default function Installation() {
-
+export default function Contribution() {
   return (
-    <div className="Installation">
-      <h1>
-          Installation page!
-      </h1>
+    <div className="Contribution">
+      <h5
+        style={{
+          color: "#023E8A",
+          marginTop: 20,
+          marginBottom: 0,
+          fontStyle: "italic",
+        }}
+      >
+        Installing PartyFish
+      </h5>
+      <br></br>
+      <img src={BlobFishParty} alt="Blobfish Party"></img>
+      <p>
+        We will have more info on how to get our game up and running once we have our first release! Stay tuned for more information.
+      </p>
     </div>
   );
 }
