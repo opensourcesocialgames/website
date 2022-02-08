f0dbe18378544f9c7a9ac10cad50d3750f90ab12
Chase Saca <chasesaca@chases-mbp.dhcp.nd.edu>
2022-02-08 12:02:25 -0500

    About Page styling updates, and fixed scaling.

--- src/containers/AboutUs.js
+++ src/containers/AboutUs.js
@@ -1,42 +1,67 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
-import NDCS from "../images/ndcs.jpg";
-import Avatar from '@mui/material/Avatar';
-import Link from '@mui/material/Link';
+import { Grid, Paper } from "@mui/material";
 
-import "./AboutUs.css"
+import NDCS from "../images/ndcs.png";
+import Avatar from "@mui/material/Avatar";
+import Link from "@mui/material/Link";
+
+import "./AboutUs.css";
 
 export default function AboutUs() {
-
   return (
     <div className="AboutUs">
       <h5
         style={{
           color: "#023E8A",
           marginTop: 20,
-          marginBottom: 0,
+          marginBottom: 15,
           fontStyle: "italic",
         }}
       >
         About the Project
       </h5>
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center">
-        <Grid item xs={6}>
-          <img src={NDCS} alt="NDCS logo"></img>
+      <Paper
+        sx={{
+          position: "relative",
+          backgroundColor: "white",
+          color: "#222f3e",
+          mb: 4,
+          mt: 1,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+        elevation={3}
+      >
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          marginTop={0}
+          spacing={1}
+        >
+          <Grid item xs={6}>
+            <img src={NDCS} alt="NDCS logo" height='80%' width='80%'></img>
+          </Grid>
+          <Grid item xs={6} paddingRight={5}>
+            <p>
+              PartyFish is an open-source project that was started as part of
+              the University of Notre Dame's CSE Open Source Software
+              Development course.
+            </p>
+            <p>
+              Throughout the course, our group of five students hopes to create
+              a fun, interactive, and easy to use open source Jackbox-style game
+              for all to enjoy!
+            </p>
+            <p>
+              We hope that you enjoy our project by playing the games we
+              developed or even contributing games of your own!
+            </p>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <p>
-            This project was started as part of the University of Notre Dame CSE Open Source Software Development course.
-          </p>
-          <p>
-            Our group of 5 students hope to create a fun and easy to use open source Jackbox-style game for all to enjoy!
-          </p>
-        </Grid>
-      </Grid>
+      </Paper>
       <h5
         style={{
           color: "#023E8A",
@@ -50,34 +75,61 @@ export default function AboutUs() {
         container
         direction="row"
         justifyContent="center"
-        alignItems="center">
+        alignItems="center"
+      >
         <Grid item xs={2}>
           <Avatar alt="Chase Saca" src="https://github.com/chase-saca.png" />
-          <Link target="_blank" href="https://github.com/chase-saca" underline="none">
+          <Link
+            target="_blank"
+            href="https://github.com/chase-saca"
+            underline="none"
+          >
             Chase Saca
           </Link>
         </Grid>
         <Grid item xs={2}>
           <Avatar alt="Abby Leone" src="https://github.com/aleone2.png" />
-          <Link target="_blank" href="https://github.com/aleone2" underline="none">
+          <Link
+            target="_blank"
+            href="https://github.com/aleone2"
+            underline="none"
+          >
             Abby Leone
           </Link>
         </Grid>
         <Grid item xs={2}>
-          <Avatar alt="Ronan McCarter" src="https://github.com/rpmccarter.png" />
-          <Link target="_blank" href="https://github.com/rpmccarter" underline="none">
+          <Avatar
+            alt="Ronan McCarter"
+            src="https://github.com/rpmccarter.png"
+          />
+          <Link
+            target="_blank"
+            href="https://github.com/rpmccarter"
+            underline="none"
+          >
             Ronan McCarter
           </Link>
         </Grid>
         <Grid item xs={2}>
-          <Avatar alt="Daniel McCarthy" src="https://github.com/dan-mccarthy265.png" />
-          <Link target="_blank" href="https://github.com/dan-mccarthy265" underline="none">
+          <Avatar
+            alt="Daniel McCarthy"
+            src="https://github.com/dan-mccarthy265.png"
+          />
+          <Link
+            target="_blank"
+            href="https://github.com/dan-mccarthy265"
+            underline="none"
+          >
             Daniel McCarthy
           </Link>
         </Grid>
         <Grid item xs={2}>
           <Avatar alt="John Gordley" src="https://github.com/jgordley.png" />
-          <Link target="_blank" href="https://github.com/jgordley" underline="none">
+          <Link
+            target="_blank"
+            href="https://github.com/jgordley"
+            underline="none"
+          >
             John Gordley
           </Link>
         </Grid>
@@ -86,14 +138,18 @@ export default function AboutUs() {
         style={{
           color: "#023E8A",
           marginTop: 50,
-          marginBottom: 30,
+          marginBottom: 20,
           fontStyle: "italic",
         }}
       >
         Contributors
       </h5>
       <p>
-        No outside contributors yet! Check out the <b>Contribute</b> page of this site for info on how to help with the project.
+        There are no outside contributors yet! Check out the{" "}
+        <a href="/contribution">
+          <b>Contribute</b>
+        </a>{" "}
+        page of this site for info on how to help with the project.
       </p>
     </div>
   );
