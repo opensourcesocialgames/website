f4aac4a9ece52050bba00b9401bfd74100ab34e9
Abby Leone <aleone2@nd.edu>
2022-02-21 10:16:22 -0500

    Few Pics Added to Screenshot Page

--- src/containers/Screenshots.js
+++ src/containers/Screenshots.js
@@ -2,7 +2,15 @@ import React from "react";
 import Box from "@mui/material/Box";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
-import BlobFishParty from "../images/blobfish.png";
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import IconButton from '@mui/material/IconButton';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import Working from "../images/screenshots/working.png"
+import Working1 from "../images/screenshots/working1.png"
+import FileLayout from "../images/screenshots/filelayout.png"
+import Redux from "../images/screenshots/redux.png"
+import Demo from "../images/screenshots/demo.png"
+import HawaiianBlob from "../images/screenshots/happyblob.png";
 
 export default function Screenshots() {
   return (
@@ -18,22 +26,34 @@ export default function Screenshots() {
         Screenshots of Development and Gameplay
       </h5>
       <br></br>
-      <img src={BlobFishParty} alt="Blobfish Party"></img>
-      <br></br>
-      <br></br>
-      <p>
-        We currently don't have any screenshots of our development or gameplay, but we will upload media once we have our first release!
-      </p>
       {itemData.length &&
         <Box justify-content={"center"}>
-          <ImageList variant="masonry" cols={3} gap={8}>
+          <ImageList variant="quilted" cols={4} rowHeight={150}>
             {itemData.map((item) => (
-              <ImageListItem key={item.img}>
+              <ImageListItem key={item.img}cols={item.cols || 1} rows={item.rows || 1}>
                 <img
-                  src={`${item.img}?w=248&fit=crop&auto=format`}
-                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  src={`${item.img}?w=${121* item.cols}&h=${1 * item.rows}&fit=crop&auto=format`}
+                  srcSet={`${item.img}?w=${121 * item.cols}&h=${1 * item.rows}&fit=crop&auto=format&dpr=2 2x`}
                   alt={item.title}
                   loading="lazy"
+                />
+                <ImageListItemBar
+                  sx={{
+                    background:
+                      'linear-gradient(to top, rgba(0,0,0,0.7) 30%, ' +
+                      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+                  }}
+                  title={item.title}
+                  position="bottom"
+                  actionIcon={
+                    <IconButton
+                      sx={{ color: 'white' }}
+                      aria-label={`star ${item.title}`}
+                    >
+                      <StarBorderIcon />
+                    </IconButton>
+                  }
+                  actionPosition="left"
                 />
               </ImageListItem>
             ))}
@@ -45,14 +65,42 @@ export default function Screenshots() {
 }
 
 const itemData = [
-  /* To add images, use the structure below. Order of the images does not matter.
+   // To add images, use the structure below. Order of the images does not matter.
+   {
+    img: Demo,
+    title: "Game Demo",
+    rows: 3,
+    cols: 4,
+   },
+   {      
+    img: Working,
+    title: "Working Hard",
+    rows: 2,
+    cols: 2,
+    },
+ /*  {
+    img: FileLayout,
+    title: "File Layout",
+    rows: 2,
+    cols: 1,
+    }, */
     {
-      img: "",
-      title: "",
+      img: HawaiianBlob,
+      title: "Party Blob",
+      rows: 2,
+      cols: 2,
     },
     {
-      img: "",
-      title: "",
+      img: Redux,
+      title: "Redux Diagram",
+      rows: 2,
+      cols: 2,
     }, 
-    */
+    {
+      img: Working1,
+      title: "Hardly Working",
+      rows: 2,
+      cols: 2,
+    },  
+
 ];
